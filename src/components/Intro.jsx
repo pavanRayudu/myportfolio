35d00c67e9b647ed2e0b9c8eb9d3cfdb38e3b9ae
Vamsi Rayudu <rpvvamsi@gmail.com>
2026-03-01 09:14:01 +0530

    ...

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -25,35 +25,34 @@ const Intro = ({ setVisible }) => {
       <section>
         <div className="content">
           <h1>Hello, I'm <span>Pavan</span></h1>
-          <p className='poppins-regular-italic'>SAP Functional Consultant, UI Developer</p>
-          <p className="address">based in Hyderabad, Telangana- India</p>
+          <p className='poppins-regular-italic'>SAP SD Functional Consultant</p>
+          <p className="address">Based in Hyderabad, Telangana- India</p>
           <p className='introduction'>Skilled and experiend SAP
             Professional having 2.5+ years of experience in IT.
             Expertise lies in Sales and Disribution module and UI developement for web apps.</p>
         </div>
         <div className="links">
           <ul>
-            <li className="link link-1">
-              <Link to='/skills' className='nav-link'>Skills</Link></li>
-            <li className="link link-2">
-              <Link to='education' className='nav-link'>Education</Link></li>
-            <li className="link link-3">
-              <Link to='/experience' className='nav-link'>Experience</Link></li>
-            <li className="link link-4">
-              <Link to='interests' className='nav-link'>Interests</Link>
-            </li>
-            <li className="link link-5">
-              <Link to='contact' className='nav-link'>Contacts</Link>
-            </li>
+
+            <Link to='/skills' className='nav-link'>Skills</Link>
+
+            <Link to='education' className='nav-link'>Education</Link>
+
+            <Link to='/experience' className='nav-link'>Experience</Link>
+
+            <Link to='interests' className='nav-link'>Interests</Link>
+
+            <Link to='contact' className='nav-link link-5'>Connect with me</Link>
+
           </ul>
-        </div>
+        </div >
 
-      </section>
-
-
+      </section >
 
 
-    </div>
+
+
+    </div >
   )
 }
 
